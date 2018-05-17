c054665db75c2c74e4372cf2b5da0097cd275a13
Jantz <jantzillac.93@gmail.com>
2018-05-17 09:41:05 -0500

    Update getItemCount to return mPlaces's item count

--- app/src/main/java/com/example/android/shushme/PlaceListAdapter.java
+++ app/src/main/java/com/example/android/shushme/PlaceListAdapter.java
@@ -84,8 +84,9 @@ public class PlaceListAdapter extends RecyclerView.Adapter<PlaceListAdapter.Plac
      */
     @Override
     public int getItemCount() {
-        // TODO (5) Update getItemCount to return mPlaces's item count
-        return 0;
+        // COMPLETED (5) Update getItemCount to return mPlaces's item count
+        if(mPlaces == null) return 0;
+        return mPlaces.getCount();
     }
 
     /**
